13effc26d921fd83a4b70b0dbdbebd4c4aad5519
alexandremalo <amalo@lanets.ca>
2015-06-07 23:13:49 -0400

    Update TriSelonMenu.java

--- src/TriSelonMenu.java
+++ src/TriSelonMenu.java
@@ -253,7 +253,7 @@ public class TriSelonMenu {
 	private static ListeDynamique TypeDeFormeInverse(ListeDynamique listATriee){
 		return listATriee;
 	}
-	//A FAIRE
+	//DONE!
 	private static ListeDynamique DistanceFormeDiagonale(ListeDynamique listATriee){
 		ListeDynamique listtrier = new ListeDynamique();
 		FormePrincipal formeATrie;
